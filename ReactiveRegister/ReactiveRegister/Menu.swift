ff9eb2773ac0941cec6afd7a76d059bbcde2b425
Yuki Yasoshima <yukiyasos@gmail.com>
2018-05-14 18:56:26 +0900

    メニューを追加

---
+++ ReactiveRegister/ReactiveRegister/Menu.swift
@@ -0,0 +1,15 @@
+//
+//  Menu.swift
+//  ReactiveRegister
+//
+//  Created by yasoshima on 2018/05/14.
+//  Copyright © 2018年 Yuki Yasoshima. All rights reserved.
+//
+
+import Foundation
+
+class Menu {
+    let price = NSDecimalNumber(string: "100")
+    let tax = NSDecimalNumber(string: "8")
+    let name = "りんご"
+}
