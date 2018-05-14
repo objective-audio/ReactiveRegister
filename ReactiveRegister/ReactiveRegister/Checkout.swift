b779cb3c2c5965f8b0594312e6e3d9c7fe237ea6
Yuki Yasoshima <yukiyasos@gmail.com>
2018-05-14 19:01:09 +0900

    Checkoutを追加

---
+++ ReactiveRegister/ReactiveRegister/Checkout.swift
@@ -0,0 +1,14 @@
+//
+//  Checkout.swift
+//  ReactiveRegister
+//
+//  Created by yasoshima on 2018/05/14.
+//  Copyright © 2018年 Yuki Yasoshima. All rights reserved.
+//
+
+import Foundation
+
+class Checkout {
+    let menu = Menu()
+    var count: Int = 1
+}
