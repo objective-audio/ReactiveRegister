701d8e09ee6fbe53b75c5ef936866af174699a2b
Yuki Yasoshima <yukiyasos@gmail.com>
2018-05-15 17:03:38 +0900

    ラベルの追加

--- ReactiveRegister/ReactiveRegister/ViewController.swift
+++ ReactiveRegister/ReactiveRegister/ViewController.swift
@@ -15,7 +15,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var clearButton: UIButton!
     @IBOutlet weak var enterButton: UIButton!
     @IBOutlet weak var amountLabel: UILabel!
+    @IBOutlet weak var taxLabel: UILabel!
     @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var paymentLabel: UILabel!
     @IBOutlet weak var stepper: UIStepper!
     
     let register: Register = AppController.shared.register
@@ -26,7 +28,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.register.numberPad.amount.asObservable().map { "\($0)" }.bind(to: self.amountLabel.rx.text).disposed(by: self.disposeBag)
+        self.register.numberPad.amount.asObservable().map { "入力 : \($0)" }.bind(to: self.amountLabel.rx.text).disposed(by: self.disposeBag)
         
         for idx in 0..<10 {
             self.numberButtons[idx].rx.tap.asSignal().map { NumberPad.Command.number(idx) }.emit(to: self.numberPad.input).disposed(by: self.disposeBag)
